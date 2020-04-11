10f8a39bc9ddec8233ec77b6a5d6a923bf703877
dyaroshevych <dyaroshevych@gmail.com>
2020-04-11 23:35:01 +0300

    added algolia location autocomplete

---
+++ src/components/ContatForm/ContactForm.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+import classes from "./ContactForm.module.scss";
+
+const ContactForm = () => {
+  return <form></form>;
+};
+
+export default ContactForm;
--- src/components/InspectionForm/InspectionForm.jsx
+++ src/components/InspectionForm/InspectionForm.jsx
@@ -1,27 +1,40 @@
-import React from "react";
+import React, { useState } from "react";
+import AlgoliaPlaces from "algolia-places-react";
 
 import { Button } from "../";
 
 import classes from "./InspectionForm.module.scss";
 
-const InspectionForm = ({ source, forwardedRef = null }) => (
-  <form method="POST" action="/" className={classes.InspectionForm}>
-    <label
-      htmlFor={`${source}-inspection-form`}
-      className={classes.InspectionForm_label}
-    >
-      Enter your address to start!
-    </label>
-    <input
-      type="email"
-      placeholder="Enter your address to start!"
-      required
-      className={classes.InspectionForm_input}
-      id={`${source}-inspection-form`}
-      ref={forwardedRef}
-    />
-    <Button size="lg">Get Inspection</Button>
-  </form>
-);
+const InspectionForm = ({ source, forwardedRef = null }) => {
+  const [address, setAddress] = useState("");
+
+  return (
+    <div className={classes.InspectionForm}>
+      <label
+        htmlFor={`${source}-inspection-form`}
+        className={classes.InspectionForm_label}
+        ref={forwardedRef}
+      >
+        Enter your address to start!
+      </label>
+      <AlgoliaPlaces
+        className={classes.InspectionForm_input}
+        id={`${source}-inspection-form`}
+        placeholder="Enter your address to start!"
+        options={{
+          appId: "pl0FDSAOXLGH",
+          apiKey: "6f6e1961877c7ac0d0573e26d3a66e42",
+          language: "en",
+          countries: ["us"],
+          type: "city",
+        }}
+        onChange={({ query, rawAnswer, suggestion, suggestionIndex }) =>
+          setAddress(suggestion.value)
+        }
+      />
+      <Button size="lg">Get Inspection</Button>
+    </div>
+  );
+};
 
 export default InspectionForm;
